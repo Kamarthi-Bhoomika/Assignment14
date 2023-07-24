b42e5decf4f40fb69dc68c6914afa78e125ed5cf
unknown <kbhoomika05@gmail.com>
2023-07-24 19:38:11 +0530

    Assignment14 Bubble Sort and Insertion Sort Analysis

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ namespace Assignment14_BubbleSort
         {
             foreach(int i in arr)
             {
-                Console.Write(i+" ");
+                Console.Write(i+", ");
             }
             Console.WriteLine("\n");
         }
@@ -60,7 +60,7 @@ namespace Assignment14_BubbleSort
 
             Random random = new Random();
             int[] arr = new int[100];
-            for(int i =0; i< 100; i++)
+            for (int i = 0; i < 100; i++)
             {
                 int num = random.Next(0, 1000);
                 arr[i] =num;
@@ -69,20 +69,21 @@ namespace Assignment14_BubbleSort
         }
         static void Main(string[] args)
         {
-            int[] arr = generateArray();
+            int[] arr1 = generateArray();
+            //int[] arr1 = { 55, 599, 985, 919, 292, 571, 9, 106, 685, 369, 944, 589, 751, 685, 535, 717, 74, 785, 908, 744, 17, 456, 108, 598, 134, 307, 205, 31, 120, 832, 183, 834, 122, 500, 217, 0, 278, 296, 110, 491, 734, 190, 636, 202, 207, 285, 853, 881, 623, 328, 525, 476, 744, 851, 918, 599, 491, 386, 784, 984, 366, 978, 985, 852, 185, 109, 466, 251, 468, 534, 439, 777, 675, 417, 10, 827, 819, 905, 391, 848, 454, 324, 407, 792, 307, 707, 90, 271, 581, 617, 509, 892, 511, 125, 125, 756, 205, 784, 16, 97 };
             Console.WriteLine("Array Before Sorting");
-            Print(arr);
-            
+            Print(arr1);
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            BubbleSort(arr);
+            BubbleSort(arr1);
             stopwatch.Stop();
             Console.WriteLine("Array After Bubble Sorting");
-            Print(arr);
-            Console.WriteLine($"Array Size {arr.Length} \tTime Take {stopwatch.Elapsed.TotalMilliseconds} milliseconds");
+            Print(arr1);
+            Console.WriteLine($"Array Size {arr1.Length} \tTime Take {stopwatch.Elapsed.TotalMilliseconds} milliseconds");
 
             Console.WriteLine("\n**********************************************");
             int[] arr2 = generateArray();
+            //int[] arr2 = { 55, 599, 985, 919, 292, 571, 9, 106, 685, 369, 944, 589, 751, 685, 535, 717, 74, 785, 908, 744, 17, 456, 108, 598, 134, 307, 205, 31, 120, 832, 183, 834, 122, 500, 217, 0, 278, 296, 110, 491, 734, 190, 636, 202, 207, 285, 853, 881, 623, 328, 525, 476, 744, 851, 918, 599, 491, 386, 784, 984, 366, 978, 985, 852, 185, 109, 466, 251, 468, 534, 439, 777, 675, 417, 10, 827, 819, 905, 391, 848, 454, 324, 407, 792, 307, 707, 90, 271, 581, 617, 509, 892, 511, 125, 125, 756, 205, 784, 16, 97 };
             Console.WriteLine("Array Before Sorting");
             Print(arr2);
             Stopwatch stopwatch2 = new Stopwatch();
